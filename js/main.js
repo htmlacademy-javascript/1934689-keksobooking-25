ae5c123dfde1f113137cb7918a19b09e007ada47
Максим Рудой <doodoo690@gmail.com>
2022-02-21 18:34:55 +0500

    Added two function agein in main.js

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-// первая функция по заданию
-function numbersRange(min, max) {
+function nubmersRange(min, max) {
   if (min >= 0 && max >= 0 && min < max) {
-    let generatedNumber = Math.trunc(min + Math.random() * (max + 1 - min));
-    console.log('Результат: ' + generatedNumber + ' из диапазона' + ' от ' + min + ' до ' + max);
-    return generatedNumber
+    const generatedNumber = Math.trunc(min + Math.random() * (max + 1 - min));
+    return generatedNumber;
   }
-  alert('Ошибка! \nДиапазон чисел должен быть:\na,b<=0 и a<b ! ');
-}
 
-numbersRange();
-// вторая функция по заданию
+}
+nubmersRange();
+
 function floatPointRange(min, max, point) {
   if (min >= 0 && max >= 0 && min < max) {
     let generatedNumber = min + Math.random() * (max + 1 - min);
     generatedNumber = generatedNumber.toFixed(point);
-    console.log('Результат: ' + generatedNumber + ' из диапазона' + ' от ' + min + ' до ' + max + 'с указанным "количеством знаков после запятой" ' + point);
     return generatedNumber;
   }
-  alert('Ошибка! \nДиапазон чисел должен быть:\na,b<=0 и a<b ! ');
+
 }
 
 floatPointRange();
