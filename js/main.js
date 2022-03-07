0d4f030bf608be3632df43194192aabda59f6242
Максим Рудой <doodoo690@gmail.com>
2022-03-07 16:37:00 +0500

    Remove const GeoPoint, added generating obj function with geodata, warpped in the function the cycle of generating objects in the array

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const MASSIV_OBJECT = [];
+
 function getRandomNumber (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -6,66 +7,87 @@ function getRandomNumber (a, b) {
   return Math.floor(result);
 }
 
-function getGeoLocation (a, b, digits = 5) {
+function getGLoc(a, b, digits = 5) {
   const lower = Math.min(Math.abs(a), Math.abs(b));
   const upper = Math.max(Math.abs(a), Math.abs(b));
   const result = Math.random() * (upper - lower) + lower;
   return +result.toFixed(digits);
 }
-const geoPointOne = getGeoLocation(35.65000, 35.70000);
-const geoPointTwo = getGeoLocation(139.70000, 139.80000);
-// console.log(geoPointTwo)
-const housing = [
+
+function getObjNav(a, b) {
+  const geoAdress = {
+    lat: a,
+    lng: b
+  };
+  return geoAdress;
+}
+
+const massivHousing = [
   'place', 'flat', 'house',
   'bungalow', 'hotel'
 ];
 
 // eslint-disable-next-line no-shadow
 function getHousing(housing){
-  return housing[Math.floor(Math.random() * housing.length)];
+  return housing[Math.floor(Math.random() *
+    housing.length)];
 }
 
-const chekTime = ['12:00', '13:00', '14:00'];
+const massivChekTime = ['12:00', '13:00', '14:00'];
 
 // eslint-disable-next-line no-shadow
 function geTime(chekTime){
-  return chekTime[Math.floor(Math.random() * chekTime.length)];
+  return chekTime[Math.floor(Math.random() *
+     chekTime.length)];
 }
 
-const features = [
+const massivFeatures = [
   'wifi', 'dishwasher', 'parking',
   'washer', 'elevator', 'conditioner'
 ];
-const featur = features.slice([0], [getRandomNumber (1, 6)]);
 
-const albumPhotos = [
+function getFeatur() {
+  massivFeatures.slice([0], [getRandomNumber (1, 6)]);
+  return massivFeatures;
+}
+
+const massivPhotos = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
-const flbumPhoto = albumPhotos.slice([0], [getRandomNumber (1, 3)]);
+function getAlbumPhoto() {
+  massivPhotos.slice([0], [getRandomNumber (1, 3)]);
+  return massivPhotos;
+}
 
-for (let i = 1; i <= 10; i++) {
-  const numberPictur = i;
-  const object = {};
-  MASSIV_OBJECT.push(object);
-  object.author = {};
-  object.author.img = numberPictur === 10 ? `img/avatars/user${numberPictur} .png` :
-    `img/avatars/user${0}${numberPictur} .png`;
-  object.offer = {};
-  object.offer.title = 'Халява';
-  object.offer.adress = `${geoPointOne}, ${geoPointTwo}` ;
-  object.offer.price = getRandomNumber(1, 100);
-  object.offer.type = getHousing(housing);
-  object.offer.rooms = getRandomNumber(1, 5);
-  object.offer.guests = getRandomNumber(1, 10);
-  object.offer.checkin = geTime(chekTime);
-  object.offer.checkout = geTime(chekTime);
-  object.offer.features = featur;
-  object.offer.description = 'просторная светлая уютная комната';
-  object.offer.photos = flbumPhoto;
-  object.location = {};
-  object.location.lat = getGeoLocation(35.65000, 35.70000);
-  object.location.lng = getGeoLocation(139.70000, 139.80000);
+// eslint-disable-next-line no-unused-vars
+function generetedOffer() {
+  for (let i = 1; i <= 10; i++) {
+    const numberPictur = i;
+    const offer = {
+      author: {
+        img: numberPictur === 10 ? `img/avatars/user${numberPictur} .png` : `img/avatars/user${0}${numberPictur} .png` },
+      offer: {
+        title:'Халява',
+        adress: getObjNav((getGLoc(35.65000, 35.70000)), (getGLoc(139.70000, 139.80000))),
+        price: getRandomNumber(1, 100,),
+        type: getHousing(massivHousing),
+        rooms: getRandomNumber(1, 5),
+        guests: getRandomNumber(1, 10),
+        checkin: geTime(massivChekTime),
+        checkout: geTime(massivChekTime),
+        features: getFeatur(),
+        description: 'просторная светлая уютная комната',
+        photos: getAlbumPhoto()
+      },
+      location: {
+        lat: getGLoc(35.65000, 35.70000),
+        lng: getGLoc(139.70000, 139.80000)
+      }
+    };
+    MASSIV_OBJECT.push(offer);
+  }
+  return MASSIV_OBJECT;
 }
