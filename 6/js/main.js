cc2bc90f21cf89356b69c1f77cdb01bc7c489b67
Keks <keks@htmlacademy.ru>
2022-03-15 20:52:32 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,35 @@
+import {getGLoc, getRandomNumber} from './util.js';
+import {getObjNav, getHousing, getTime, getFeatur, getAlbumPhoto, massivChekTime, massivHousing} from './data.js';
+import './generatedPopup.js';
+// eslint-disable-next-line no-unused-vars
+function generatedOffer() {
+  const massivObject = [];
+  for (let i = 1; i <= 10; i++) {
+    const numberPictur = i;
+    const offer = {
+      author: {
+        img: numberPictur === 10 ? `img/avatars/user${numberPictur}.png` : `img/avatars/user${0}${numberPictur}.png` },
+      offer: {
+        title:'Халява',
+        adress: getObjNav((getGLoc(35.65000, 35.70000)), (getGLoc(139.70000, 139.80000))),
+        price: getRandomNumber(1, 100,),
+        type: getHousing(massivHousing),
+        rooms: getRandomNumber(1, 5),
+        guests: getRandomNumber(1, 10),
+        checkin: getTime(massivChekTime),
+        checkout: getTime(massivChekTime),
+        features: getFeatur(),
+        description: 'просторная светлая уютная комната',
+        photos: getAlbumPhoto()
+      },
+      location: {
+        lat: getGLoc(35.65000, 35.70000),
+        lng: getGLoc(139.70000, 139.80000)
+      }
+    };
+    massivObject.push(offer);
+  }
+  return massivObject;
+}
+generatedOffer();
+export {generatedOffer};
